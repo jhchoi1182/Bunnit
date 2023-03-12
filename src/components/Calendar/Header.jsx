2ca58e41bf0a75bf76d80f6563dfb05056e735ac
jhchoi1182 <jhchoi1182@gmail.com>
2023-03-13 02:19:09 +0900

    Level 2 :♻️달력 헤더에 컴파운드 컴포넌트 패턴 적용

--- src/components/Calendar/Header.jsx
+++ src/components/Calendar/Header.jsx
@@ -1,48 +1,15 @@
-import { Text, TouchableOpacity, View } from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { useContext } from "react";
-import { CalendarContext } from "./CalendarContainer";
+import { CalenderHeader } from "./HeaderParts/HeaderParts";
 
 const Header = () => {
-  const { selectedYear, selectedMonth, prevMonth, nextMonth } = useContext(CalendarContext);
-
-  const monthNames = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-
   return (
-    <View style={styles.header}>
-      <TouchableOpacity onPress={prevMonth}>
-        <MaterialCommunityIcons name="chevron-left" size={30} color="#4bcffa" />
-      </TouchableOpacity>
-      <Text style={{ fontWeight: 700 }}>
-        {monthNames[selectedMonth - 1]} {selectedYear}
-      </Text>
-      <TouchableOpacity onPress={nextMonth}>
-        <MaterialCommunityIcons name="chevron-right" size={30} color="#4bcffa" />
-      </TouchableOpacity>
-    </View>
+    <CalenderHeader>
+      <CalenderHeader.SpaceBetween>
+        <CalenderHeader.prevMonth />
+        <CalenderHeader.DateText />
+        <CalenderHeader.nextMonth />
+      </CalenderHeader.SpaceBetween>
+    </CalenderHeader>
   );
 };
 
 export default Header;
-
-const styles = {
-  header: {
-    flex: 0.15,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-};
---
+++ src/components/Calendar/HeaderParts/HeaderParts.jsx
@@ -0,0 +1,81 @@
+import { Text, TouchableOpacity, View } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { CalendarContext } from "../../../Hooks.js/Hook";
+import { useContext } from "react";
+
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+// 헤더 베이스
+
+export const CalenderHeader = ({ children }) => {
+  return <View style={{ flex: 0.15, justifyContent: "center" }}>{children}</View>;
+};
+
+// 레이아웃
+
+const SpaceBetween = ({ children }) => {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      {children}
+    </View>
+  );
+};
+
+// 이전 달 버튼
+
+const prevMonth = () => {
+  const { prevMonth } = useContext(CalendarContext);
+  return (
+    <TouchableOpacity onPress={prevMonth} style={{}}>
+      <MaterialCommunityIcons name="chevron-left" size={30} color="#4bcffa" />
+    </TouchableOpacity>
+  );
+};
+
+// 다음 달 버튼
+
+const nextMonth = ({}) => {
+  const { nextMonth } = useContext(CalendarContext);
+  return (
+    <TouchableOpacity onPress={nextMonth}>
+      <MaterialCommunityIcons name="chevron-right" size={30} color="#4bcffa" />
+    </TouchableOpacity>
+  );
+};
+
+// 표시될 날짜
+
+const DateText = () => {
+  const { selectedMonth, selectedYear } = useContext(CalendarContext);
+  return (
+    <Text style={{ fontWeight: 700 }}>
+      {monthNames[selectedMonth - 1]} {selectedYear}
+    </Text>
+  );
+};
+
+// 명시적 재선언
+
+CalenderHeader.SpaceBetween = SpaceBetween;
+CalenderHeader.prevMonth = prevMonth;
+CalenderHeader.nextMonth = nextMonth;
+CalenderHeader.DateText = DateText;
